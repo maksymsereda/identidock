fed4e568f44c24f6d12a6c383b1e6b26451c54ae
Maksym Sereda <sereda206@gmail.com>
2019-04-09 09:57:56 +0200

    Added prod file, proxy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Starting identidock system"
+
+docker run -d --restart=always --name redis redis
+docker run -d --restart=always --name dnmonster amouat/dnmonster
+docker run -d --restart=always \
+  --link dnmonster:dnmonster \
+  --link redis:redis \
+  -e ENV=PROD \
+  --name identidock msereda/identidock:newest
+docker run -d --restart=always \
+  --name proxy \
+  --link identidock:identidock \
+  -p 80:80 \
+  -e NGINX_HOST=0.0.0.0 \
+  -e NGINX_PROXY=http://identidock:9090 \
+  msereda/proxy:1.0
+
+echo "Started"
