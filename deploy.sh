9d806161957ab379924a76edfc52e020f6a5097d
Maksym Sereda <sereda206@gmail.com>
2019-04-29 11:16:24 +0200

    Added Logstash

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,19 @@ set -e
 
 echo "Starting identidock system"
 
-docker run -d --restart=always --name redis redis
-docker run -d --restart=always --name dnmonster amouat/dnmonster
-docker run -d --restart=always \
+docker run -d --restart=on-failure:5 --name redis redis
+docker run -d --restart=on-failure:5 --name dnmonster amouat/dnmonster
+docker run -d --restart=on-failure:5 \
   --link dnmonster:dnmonster \
   --link redis:redis \
   -e ENV=PROD \
   --name identidock msereda/identidock:newest
-docker run -d --restart=always \
+docker run -d --restart=on-failure:5 \
   --name proxy \
   --link identidock:identidock \
   -p 80:80 \
-  -e NGINX_HOST=0.0.0.0 \
+  -e NGINX_HOST=192.168.0.11 \
   -e NGINX_PROXY=http://identidock:9090 \
-  msereda/proxy:1.0
+  msereda/proxy:newest
 
 echo "Started"
